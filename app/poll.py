d65dc0cd11a13a7776a67104892b43d90feb9855
Patrick Scholten <pydevpat@gmail.com>
2019-05-10 16:26:25 +0200

    session configuration is now in seperate yaml file

--- app/poll.py
+++ app/poll.py
@@ -2,29 +2,29 @@ from flask import Flask, render_template, request, redirect, make_response
 from flask_bootstrap import Bootstrap
 import json
 import datetime
+import yaml
 
+
+config_file = 'topics.yaml'
+results_file = 'results.txt'
+feedback_list = []
 app = Flask(__name__, static_url_path='/static')
 Bootstrap(app)
 
-values = ['1', '2', '3', '4', '5']
-topic1 = {'id': 't1',
-          'title': 'Can the Cloud be Secure?! (Renato Kuiper, Tesorion)'}
-topic2 = {'id': 't2',
-          'title': 'Cyber Threat Intelligence (Michael Jones, ING)'}
-topic3 = {'id': 't3',
-          'title': 'Hack#ING workshop: with live OWASP labs (Glenn ten Cate, '
-                   'ING)'}
-topic4 = {'id': 't4',
-          'title': 'Overall pizza session: Another Slice of Security'}
-topics = [topic1, topic2, topic3, topic4]
 
-filename = 'data.txt'
-feedback_list = []
-
-
-@app.route('/')
-def root():
-    return render_template('poll.html', topics=topics, values=values)
+def load_config(filename):
+    topics = []
+    config = yaml.load(open(filename), Loader=yaml.FullLoader)
+    for item in config['session_config']:
+        if item['type'] == 'talk':
+            title = (item['title'] + ' (' + item['speaker'] + ', '
+                     + item['company'] + ')')
+            topic = {'id': item['id'], 'title': title}
+            topics.append(topic)
+        elif item['type'] == 'session':
+            topic = {'id': item['id'], 'title': item['title']}
+            topics.append(topic)
+    return topics
 
 
 def getcookie():
@@ -35,6 +35,12 @@ def getcookie():
         return False
 
 
+@app.route('/')
+def root():
+    values = ['1', '2', '3', '4', '5']
+    return render_template('poll.html', topics=topics, values=values)
+
+
 @app.route('/poll')
 def poll():
 
@@ -42,7 +48,7 @@ def poll():
     print(feedback)
     if not getcookie():
         feedback_list.append(feedback)
-        with open(filename, mode='w') as my_file:
+        with open(results_file, mode='w') as my_file:
             my_file.write(json.dumps(feedback_list) + "\n")
         response = make_response(redirect('/thankyou'))
         response.set_cookie('quest4dave', "banaan",
@@ -67,30 +73,25 @@ def alreadyvoted():
 
 @app.route('/results')
 def show_results():
-    votes = {}
-    topic1_score = 0
-    topic2_score = 0
-    topic3_score = 0
-    topic4_score = 0
+    score = {}
+    for t in topics:
+        score[t['id']] = 0
     counter = 0
     remarks = []
-    for val in values:
-        votes[val] = 0
-
+    scores = []
     try:
-        with open(filename, 'r') as read_file:
+        with open(results_file, 'r') as read_file:
             feedback_list = json.load(read_file)
         for feedback in feedback_list:
-            topic1_score += int(feedback[topic1['id']])
-            topic2_score += int(feedback[topic2['id']])
-            topic3_score += int(feedback[topic3['id']])
-            topic4_score += int(feedback[topic4['id']])
+            for t in topics:
+                score[t['id']] += int(feedback[t['id']])
+            if feedback['textarea1'] == '':
+                remarks.append('-')
+            else:
+                remarks.append(feedback['textarea1'])
             counter += 1
-            remarks.append(feedback['textarea1'])
-        scores = [{'topic': topic1['title'], 'score': topic1_score},
-                  {'topic': topic2['title'], 'score': topic2_score},
-                  {'topic': topic3['title'], 'score': topic3_score},
-                  {'topic': topic4['title'], 'score': topic4_score}]
+        for t in topics:
+            scores.append({'topic': t['title'], 'score': score[t['id']]})
     except json.decoder.JSONDecodeError:
         scores = []
     return render_template('results.html', scores=scores,
@@ -98,4 +99,5 @@ def show_results():
 
 
 if __name__ == "__main__":
+    topics = load_config(config_file)
     app.run(debug=True, host='0.0.0.0')
